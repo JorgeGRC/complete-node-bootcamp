4a22307c67023d3f7d90e31b05bc7382e0f7fb07
Jorge Garcia Cuervo <jorge@Jorges-MacBook-Air.local>
2023-10-30 20:46:20 +0100

    async exercise

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -2,10 +2,38 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 😅');
 
-  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).end((err, res) => {
-    console.log(res.body);
+      // this 'data'arg will be passed in the 'then' handler
+      resolve(data);
+    });
   });
-});
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😅');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog written to file!');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
