e09cd2c6b14228ef6a527571d9a1ff2e4ed38dfc
Jorge Garcia Cuervo <jorge@Jorges-MacBook-Air.local>
2023-10-30 19:41:05 +0100

    lecture on modules

---
+++ 2-how-node-works/starter/test-module-1.js
@@ -0,0 +1,16 @@
+class Calculator {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+}
+
+// We export only one value - we use module.exports
+module.exports = Calculator;
